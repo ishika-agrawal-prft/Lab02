56b836f41c6256624ba85fdbbd19e02515451966
Ishika Agrawal <ishika.agrawal@perficient.com>
2026-06-04 10:30:46 +0530

    Sub feature added

--- calc.py
+++ calc.py
@@ -2,6 +2,8 @@
 def add(a,b):
     return a+b
 
+def subtract(a,b):
+    return a-b
 
 n = int(input("Enter a number: "))
 m = int(input("Enter another number: "))
@@ -9,5 +11,7 @@ option = input("Choose an operation (+, -, *, /): ")
 
 if option == "+":
     result = add(n,m)
+elif option == "-":
+    result = subtract(n,m)
 
 print(f"The result of {n} + {m} is: {result}")
