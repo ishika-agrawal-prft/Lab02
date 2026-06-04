adb8cc155600e789bd94f0ac12d00be11621a7da
Ishika Agrawal <ishika.agrawal@perficient.com>
2026-06-04 10:43:39 +0530

    div

--- calc.py
+++ calc.py
@@ -8,6 +8,11 @@ def subtract(a,b):
 def multiply(a,b):
     return a*b
 
+def divide(a,b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a/b
+
 n = int(input("Enter a number: "))
 m = int(input("Enter another number: "))
 option = input("Choose an operation (+, -, *, /): ")
@@ -18,5 +23,7 @@ elif option == "-":
     result = subtract(n,m)
 elif option == "*":
     result = multiply(n,m)
+elif option == "/":
+    result = divide(n,m)
 
 print(f"The result of {n} + {m} is: {result}")
